65fce48d5e4538cad692cd603b91dc9991849e9b
ibroid <maulana858@gmail.com>
2024-09-24 13:46:25 +0700

    fix bug muncul semua pengajuan

--- application/core/R_Controller.php
+++ application/core/R_Controller.php
@@ -10,7 +10,7 @@ class R_Controller extends CI_Controller
 
     public CI_DB $database;
 
-    public PegawaiEntity $pegawai;
+    public PegawaiEntity|null $pegawai;
 
     public Addons $addons;
 
@@ -38,9 +38,8 @@ class R_Controller extends CI_Controller
         $this->user = $this->session->userdata('user_login');
         $this->load->database();
         $this->load->library('EloquentDatabase', null, 'ed');
-        if ($this->user["level"] == "Operator") {
-            $this->pegawai = PegawaiEntity::find($this->user['profile']['pegawai_id']);
-        } else {
+        $this->pegawai = PegawaiEntity::find($this->user['profile']['pegawai_id']);
+        if ($this->user["level"] == "Admin" || $this->user["level"] == "Developer") {
             $this->is_admin = true;
         }
     }
